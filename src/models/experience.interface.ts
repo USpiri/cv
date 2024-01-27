eee108ad731b68abc65abd51b1916190da3f4346
USpiri <urielspiridione135@gmail.com>
2024-01-27 17:48:07 -0300

    ✨ Feat: Add project and experiences interfaces

---
+++ src/models/experience.interface.ts
@@ -0,0 +1,9 @@
+export interface Experience {
+  id: string;
+  date: string;
+  type?: string;
+  company: string;
+  job: string;
+  url?: string;
+  description: string;
+}
---
+++ src/models/project.interface.ts
@@ -0,0 +1,8 @@
+export interface Project {
+  id: string;
+  title: string;
+  tags: string[];
+  description: string;
+  url?: string;
+  repo?: string;
+}
